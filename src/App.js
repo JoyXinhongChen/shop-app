65a31dfabac7fcdd9cffc0899b20df0db8bfbe20
joy <joychen0328@gmail.com>
2020-06-16 13:44:45 +1000

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import {setCurrentUser} from './redux/user/user.actions';
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      currentUser: null
-    }
-  }
+
 unsubscribeFromAuth=null;
 
   componentDidMount(){
+
+    const {setCurrentUser} =this.props;
+
    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
      //this.setState({currentUser:user});
      //createUserProfileDocument(user);
@@ -24,21 +23,16 @@ unsubscribeFromAuth=null;
     if(userAuth){
       const userRef = await createUserProfileDocument(userAuth);
       userRef.onSnapshot(snapShot =>{
-       this.setState({
-         currentUser:{
+       this.props.setCurrentUser({
            id:snapShot.id,
            ...snapShot.data()
-         }
-        // },()=>{
-        //   console.log(this.state);
        });
-      // console.log(this.state);
-
       });
 
-    }else{
-      this.setState({currentUser: userAuth});
     }
+    //else{
+      setCurrentUser( userAuth);
+  //  }
     })
   }
   componentWillUnmount(){
@@ -48,7 +42,7 @@ unsubscribeFromAuth=null;
   render(){
   return (
     <div >
-    <Header currentUser={this.state.currentUser}/>
+    <Header />
   <Switch>
 <Route exact path='/' component={HomePage} />
 <Route path='/shop' component={ShopPage} />
@@ -58,4 +52,7 @@ unsubscribeFromAuth=null;
   );}
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser:user => dispatch(setCurrentUser(user))
+});
+export default connect(null,mapDispatchToProps)(App);
---
+++ src/test.js
@@ -0,0 +1,16 @@
+//joy notesgit 
+
+
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+
+
+
+// const firestore=firebase.firestore();
+
+// firestore.collection('users').doc('');
+// firestore.doc('');
+// firestore.collection('');
+
+
+
